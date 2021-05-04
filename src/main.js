409be050869bf4adeebf54fb6af35d35a78a3a07
Paul Clark <clark.paul123@gmail.com>
2021-05-04 19:19:21 +0100

    Add new post functionality

--- src/main.js
+++ src/main.js
@@ -1,11 +1,31 @@
-import { ui } from './ui'
+import { ui } from './ui';
 
 const getPosts = async () => {
   const posts = await fetch('http://localhost:3000/posts')
-    .then(res => res.json())
+    .then(res => res.json());
 
-  return ui.showPosts(posts)
-}
-
+    return ui.showPosts(posts);
+  }
+  
 document.addEventListener('DOMContentLoaded', getPosts);
 
+const submitPost = async () => {
+  const title = document.querySelector('#title').value;
+  const body = document.querySelector('#body').value;
+
+  const postData = { title, body };
+
+  const response = await fetch('http://localhost:3000/posts', {
+    method: 'POST',
+    headers: {
+      'Accept': 'application/json',
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify(postData)
+  })
+    .then(res => getPosts())
+    .catch(err => console.log(err));
+}
+
+document.querySelector('#post-submit-btn')
+  .addEventListener('click', submitPost);
